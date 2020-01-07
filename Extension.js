de70e9196f10b35f65e01ac6fab0577a846fee99
Andaleeb Khogaly <58003961+andaleebkhog@users.noreply.github.com>
2020-01-07 16:50:28 +0200

    Add files via upload

---
+++ Extension.js
@@ -0,0 +1,14 @@
+number1 = 3;
+number2 = 2.9;
+number3 = 90;
+
+// a console.log(firstName); gave me a warning
+ console.log(firstName); //b gave me an undefined value
+var firstName = "Andaleeb";
+firstName[3] = 'A';
+console.log(firstName); // wasn't changed ya lebo
+lastName = "Anan";
+
+numbers = [2,7,3,4,20,9];
+flag = true;
+console.log("This is the JavaScript External file");
